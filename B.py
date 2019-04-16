f8087dcc82e376c4462e76f3a79b0f1de4e7ef48
williamabreu <williamsantos_00@hotmail.com>
2019-04-16 12:18:42 -0300

    Adicionada classe B

---
+++ B.py
@@ -0,0 +1,22 @@
+class B:
+    def __init__(self, B1: int, B2: float):
+        self.B1 = B1
+        self.B2 = B2
+    
+    def get_B1(self):
+        return self.B1
+    
+    def get_B2(self):
+        return self.B2
+    
+    def set_B1(self, B1: int):
+        self.B1 = B1
+    
+    def set_B2(self, B2: float):
+        self.B2 = B2
+    
+    def MB1(self):
+        print('MB1')
+    
+    def MB2(self):
+        print('MB2')
