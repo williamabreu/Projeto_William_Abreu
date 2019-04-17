54d8bfac5afac5f87d63a94f40dc071c480eb62f
williamabreu <williamsantos_00@hotmail.com>
2019-04-17 00:55:56 -0300

    Criação da classe C

---
+++ C.py
@@ -0,0 +1,10 @@
+class C:
+    def __init__(self, C1: str, C2: int):
+        self.C1 = C1
+        self.C2 = C2
+    
+    def MC1(self):
+        print('MC1')
+    
+    def MC2(self):
+        print('MC2')
